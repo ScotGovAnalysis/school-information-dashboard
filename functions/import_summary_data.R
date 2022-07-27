f1de829b375b2fc7557cf546ec3b5965ae6f14e4
alicebyers5 <alice.byers@gov.scot>
2022-07-27 12:06:16 +0100

    Capitalise school_type in summary data

--- functions/import_summary_data.R
+++ functions/import_summary_data.R
@@ -32,18 +32,22 @@ import_summary_data <- function(sheet_name, calendar_year) {
     stop("File does not exist:\n", filepath, ".")
   }
   
-  # Read in data and clean column names
-  
+  # Read in data
   here::here("data", "school_summary_statistics", 
              paste0(calendar_year, "_school_summary_statistics.xlsx")) %>%
     readxl::read_excel(sheet = sheet_name, col_types = "text") %>%
+    
+    # Clean column names
     janitor::clean_names() %>%
     dplyr::rename_with(~ "female", tidyselect::matches("^f$")) %>%
     dplyr::rename_with(~ "male", tidyselect::matches("^m$")) %>%
     dplyr::rename_with(~ "fte_teacher_numbers", 
                        tidyselect::matches("^fte$")) %>%
     dplyr::rename_with(~ "fsm", tidyselect::matches("^universal_fsm$")) %>%
-    dplyr::rename_with(~ "no_fsm", tidyselect::matches("^other_fsm$"))
+    dplyr::rename_with(~ "no_fsm", tidyselect::matches("^other_fsm$")) %>%
+    
+    # Ensure school_type is capatilised
+    dplyr::mutate(across(any_of("school_type"), ~ stringr::str_to_title(.)))
   
 }
 
