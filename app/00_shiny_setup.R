6af69b56ae9a835b43ab30e898b208d7997ee65c
alicebyers5 <alice.byers@gov.scot>
2022-08-22 11:57:32 +0100

    Add script header and update filepaths relative to app/

--- app/00_shiny_setup.R
+++ app/00_shiny_setup.R
@@ -1,6 +1,31 @@
+#########################################################################
+# Name of file - 00_shiny_setup.R
+# Data release - School Information Dashboard
+#
+# Type - Reproducible Analytical Pipeline (RAP)
+# Written/run on - RStudio Desktop
+# Version of R - 3.6.3
+#
+# Description - Sets up environment required for running SID shiny apps. 
+# This is the only file which requires manual changes before the 
+# shiny apps are run. This script does not need to be run separately.
+#
+# NOTE - The filepaths in this script are relative to the app/ folder. 
+# This means they will work when this script is run within the shiny app, 
+# but not if run directly from this script.
+#########################################################################
 
 
-# Load packages
+### 1 - Run Label - TO UPDATE ----
+
+## The run label for the data to be used in the shiny apps. This label should
+## match a folder in all of the following; app/primary_data/, 
+## app/secondary_data/, app/special_data/.
+
+shiny_run_label <- "2022-July"
+
+
+### 2 - Load packages ----
 
 library(shiny)
 library(dplyr)
@@ -22,30 +47,22 @@ library(stringr)
 library(shinycssloaders)
 
 
-# Turn off scientific notation format
+### 3 - Turn off scientific notation format ----
 
 options(scipen = 999)
 
 
-# Set run_label to define which data to use
+### 4 - Load shiny modules ----
 
-shiny_run_label <- "2022-July"
-
-
-# Load ggplot theme and modules
+walk(list.files("modules", pattern = "\\.R$", full.names = TRUE), source)
+walk(list.files("modules/text_content", pattern = "\\.R$", full.names = TRUE), source)
 
 
-walk(list.files(here("app", "modules"), pattern = "\\.R$", full.names = TRUE), source)
-walk(list.files(here("app", "modules", "text_content"), pattern = "\\.R$", full.names = TRUE), source)
-
-
-# Set default ggplot themes and colours
+### 5 - Set default ggplot themes and colours ----
 
 theme_set(sid_theme())
 update_geom_defaults("line", list(colour = "#3182bd", size = 1))
 update_geom_defaults("col", list(colour = "#3182bd", fill = "#3182bd"))
-
-
 
 
 ### END OF SCRIPT ###
