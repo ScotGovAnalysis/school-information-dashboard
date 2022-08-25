2c3573aa76e216ead4f508d2f3a839c9104e8c80
alicebyers5 <alice.byers@gov.scot>
2022-08-25 15:05:25 +0100

    Change lat/long variable names

--- app/modules/map_module.R
+++ app/modules/map_module.R
@@ -18,7 +18,7 @@ map_server <- function(input, output, session, data) {
       setView(-4.140078, 57.837990, zoom = 5.3) %>%
       addCircleMarkers(
         data = data(),
-        lng = ~ lng, lat = ~ lat,
+        lng = ~ longitude, lat = ~ latitude,
         radius = 6,
         stroke = FALSE,
         opacity = 1,
