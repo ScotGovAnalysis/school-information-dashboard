80e367182b97607c1463a74bc2cf439b09ea5c66
alicebyers5 <alice.byers@gov.scot>
2022-09-07 17:08:12 +0100

    Refine error message and display for no-data error class only

--- app/modules/html_tags_module.R
+++ app/modules/html_tags_module.R
@@ -1,21 +1,19 @@
 html_tags <- function() {
   
-  tagList(
-    
-    # Recode any error messages that are visible to user
-    tags$style(
-      type = "text/css",
-      ".shiny-output-error { visibility: hidden; }",
-      paste0(".shiny-output-error:before { visibility: visible; ",
-             "content: 'There is no data for this chart'; }")
-    ),
+  tags$head( 
     
     # Set universal font
     # This is used as a workaround for ggiraph issue where font is different
     # when app is deployed:
     # https://github.com/davidgohel/ggiraph/issues/91
-    tags$head( 
-      tags$style(type = "text/css", "text {font-family: sans-serif}")
+    tags$style(type = "text/css", "text {font-family: sans-serif}"),
+    
+    # Recode error messages resulting from data validation
+    tags$style(
+      type = "text/css",
+      ".shiny-output-error-no-data { visibility: hidden; }",
+      ".shiny-output-error-no-data:before { visibility: visible; ",
+      "content: 'There is no data available for this chart.'; }"
     )
     
   )
